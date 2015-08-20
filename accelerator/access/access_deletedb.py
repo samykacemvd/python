a086ab9b473c801088de4f4eed93fe5ebe9289a1
tallentinformatique <samy.kacem@tallentinformatique.ch>
2015-08-20 16:53:49 +0200

    Create access_deletedb.py

---
+++ accelerator/access/access_deletedb.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# tested on python 3.4
+# author : Samy Kacem
+# Only on PC!!!!!!
+# Install first https://pypi.python.org/pypi/pypyodbc
+
+# Easy as a pie. I'm using here the os lib
+
+import os;
+
+os.remove("C:\\base.accdb");
